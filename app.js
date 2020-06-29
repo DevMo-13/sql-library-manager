5befb90c0065c5c72d2eebee7d35a921ab79034e
DevMo-13 <moniquecinatl@gmail.com>
2020-06-29 11:09:11 -0700

    Updated error handling

--- app.js
+++ app.js
@@ -18,11 +18,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Middleware.
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// Routes.
 app.use('/', routes);
 app.use('/books', books);
 
@@ -39,12 +40,12 @@ app.use( (err, req, res, next) => {
   res.status(err.status || 500);
   if (err.status === 404) {
     console.log(err);
-    res.render('books/page-not-found', { err, title: 'Page Not Found' });
+    res.render('pageNotFound', { err, title: 'Page Not Found' });
   } else {
     const err = new Error();
     err.status = 500;
     console.log(err);
-    res.render('books/error', { err, title: 'Server Error' });
+    res.render('error', { err, title: 'Server Error' });
   }
 });
 
